657eb20a7310d0ae0d06517bbf87d0d7159c5afa
namooTH <73419369+namooTH@users.noreply.github.com>
2023-05-20 18:42:26 +0700

    bugs fix

--- bot.py
+++ bot.py
@@ -5,12 +5,15 @@ from discord import Webhook
 import aiohttp
 import asyncio
 
-token = "<insert token here>"
-url = "<insert webhook url here>"
-endchannel = "<insert your channel u want bot to send here>"
+token = "<user token>"
+url = "<webhook url>"
+endchannel = <your channel u want the bot to take command>
 
+
+# do not mess with this
 notallowid = []
-
+lastperson = ""
+# do not mess with this
 
 bot = commands.Bot(command_prefix='o!')
 bot.remove_command('help') # // this line is important do not delete ///
@@ -19,30 +22,43 @@ logging.basicConfig(level=logging.DEBUG)
 
 @bot.listen("on_message")
 async def on_message(message: discord.Message):
+    global lastperson
     if message.channel.id == 930842597759541328:
       async with aiohttp.ClientSession() as session:
         webhook = Webhook.from_url(url, session=session)
         msgurl = ""
-        file = open("list.txt", "r")
-        lines = file.readlines()
-        allmessid = {}
-        for i in lines:
-            i = i.replace("\n", "")
-            i = i.split("|")
-            allmessid.update({i[0]: i[1]})
-        if message.reference:
-            if str(message.reference.resolved.id) in allmessid.values():
-                for k,v in allmessid.items():
-                    if v == str(message.reference.resolved.id):
-                        attic = bot.get_channel(endchannel)
-                        msg = await attic.fetch_message(int(k))
-                        msgurl = msg.jump_url
+        try:
+            file = open("list.txt", "r")
+            lines = file.readlines()
+            allmessid = {}
+            for i in lines:
+                i = i.replace("\n", "")
+                i = i.split("|")
+                allmessid.update({i[0]: i[1]})
+            if message.reference:
+                if str(message.reference.resolved.id) in allmessid.values():
+                    for k,v in allmessid.items():
+                        if v == str(message.reference.resolved.id):
+                            attic = bot.get_channel(endchannel)
+                            msg = await attic.fetch_message(int(k))
+                            msgurl = msg.jump_url
+        except:
+            msgurl = "Reducted"
+
+        #// when the user dont have avatar it would just error so this is the fix
+        try:
+            avatar_url=(message.author.avatar.url if message.author.avatar else None)
+        except:
+            avatar_url=None
+        #// when the user dont have avatar it would just error so this is the fix
+
         wh_mess = await webhook.send((f"[Replied to {message.reference.resolved.author}](<{msgurl}>)\n" if message.reference else "") # reply hyperlink
-                                    + f"[Jump to message](<{message.jump_url}>)\n"
-                                    + f"{message.content if message.content else ''}", # the message receives should have attachments of message.contnet is none
+                                    + (f"[Jump to message](<{message.jump_url}>)\n" if lastperson != message.author else "")
+                                    + (f"{message.content if message.content else ''}"), # the message receives should have attachments of message.contnet is none
                                     #replaced id with storage system
                                     files=([await a.to_file() for a in message.attachments] if message.attachments else []), 
-                                    avatar_url= message.author.avatar.url,username=str(message.author), allowed_mentions=discord.AllowedMentions.none(), wait=True)
+                                    avatar_url=avatar_url, allowed_mentions=discord.AllowedMentions.none(), wait=True)
+        lastperson = message.author
         file = open("list.txt", 'a') #appeding the id to txt
         file.write(f"{wh_mess.id}|{message.id}\n")
         file.close()

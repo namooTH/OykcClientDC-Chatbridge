bdfb3173f599a44c7f251d492630e08eb98b12bd
namooTH <73419369+namooTH@users.noreply.github.com>
2023-05-21 12:56:46 +0700

    improvement

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from discord import Webhook
 import aiohttp
 import asyncio
 
-token = "<user token>"
-url = "<webhook url>"
-endchannel = <your channel u want the bot to take command>
+token = "" # user token
+url = "" # webhook url
+endchannel = 0 # < insert channel id that u want the command to be execute
 
 
 # do not mess with this
@@ -20,6 +20,12 @@ bot.remove_command('help') # // this line is important do not delete ///
 
 logging.basicConfig(level=logging.DEBUG)
 
+async def sendwebhook(content):
+    async with aiohttp.ClientSession() as session:
+        webhook = Webhook.from_url(url, session=session)
+        await webhook.send(content=content)
+
+
 @bot.listen("on_message")
 async def on_message(message: discord.Message):
     global lastperson
@@ -27,24 +33,20 @@ async def on_message(message: discord.Message):
       async with aiohttp.ClientSession() as session:
         webhook = Webhook.from_url(url, session=session)
         msgurl = ""
-        try:
-            file = open("list.txt", "r")
-            lines = file.readlines()
-            allmessid = {}
-            for i in lines:
-                i = i.replace("\n", "")
-                i = i.split("|")
-                allmessid.update({i[0]: i[1]})
-            if message.reference:
-                if str(message.reference.resolved.id) in allmessid.values():
-                    for k,v in allmessid.items():
-                        if v == str(message.reference.resolved.id):
-                            attic = bot.get_channel(endchannel)
-                            msg = await attic.fetch_message(int(k))
-                            msgurl = msg.jump_url
-        except:
-            msgurl = "Reducted"
-
+        file = open("list.txt", "r")
+        lines = file.readlines()
+        allmessid = {}
+        for i in lines:
+            i = i.replace("\n", "")
+            i = i.split("|")
+            allmessid.update({i[0]: i[1]})
+        if message.reference:
+            if str(message.reference.resolved.id) in allmessid.values():
+                for k,v in allmessid.items():
+                    if v == str(message.reference.resolved.id):
+                        attic = bot.get_channel(endchannel)
+                        msg = await attic.fetch_message(int(k))
+                        msgurl = msg.jump_url
 
         #// when the user dont have avatar it would just error so this is the fix
         try:
@@ -58,7 +60,7 @@ async def on_message(message: discord.Message):
                                     + (f"{message.content if message.content else ''}"), # the message receives should have attachments of message.contnet is none
                                     #replaced id with storage system
                                     files=([await a.to_file() for a in message.attachments] if message.attachments else []), 
-                                    avatar_url=avatar_url,username=str(message.author), allowed_mentions=discord.AllowedMentions.none(), wait=True)
+                                    avatar_url=avatar_url,username=str(message.author), allowed_mentions=discord.AllowedMentions.none(), embed=(message.embeds[0] if message.embeds else None), wait=True)
         lastperson = message.author
         file = open("list.txt", 'a') #appeding the id to txt
         file.write(f"{wh_mess.id}|{message.id}\n")
@@ -71,14 +73,19 @@ async def on_message(message: discord.Message):
             if message.content.startswith("!"):
                 if not message.author.bot:
                     if not message.author == bot.user:
-                        if message.author.id not in notallowid:
-                            onixgeneral = bot.get_channel(930842597759541328)
-                            await onixgeneral.send((message.content)[1:], files=([await a.to_file() for a in message.attachments] if message.attachments else [])) #stole rasp code ong
-                            notallowid.append(message.author.id)
-                            await asyncio.sleep(5)
-                            notallowid.remove(message.author.id)
-                        else:
+                        try:
+                            if message.author.id not in notallowid:
+                                onixgeneral = bot.get_channel(930842597759541328)
+                                await onixgeneral.send((message.content)[1:], files=([await a.to_file() for a in message.attachments] if message.attachments else [])) #stole rasp code ong
+                                notallowid.append(message.author.id)
+                                await asyncio.sleep(5)
+                                notallowid.remove(message.author.id)
+                            else:
+                                await message.add_reaction("❌")
+                        except Exception as e:
+                          async with aiohttp.ClientSession() as session:
                             await message.add_reaction("❌")
+                            await sendwebhook(content=f"```{e}```")
 
     # delete system
 
@@ -87,12 +94,10 @@ async def on_message(message: discord.Message):
             if message.reference.resolved.author.bot:
                 if message.content == ".del":
                     try:
-                        file = open("list.txt", "r")
-                        lines = file.readlines()
+                        lines = open("list.txt", "r").readlines()
                         allmessid = {}
                         for i in lines:
-                            i = i.replace("\n", "")
-                            i = i.split("|")
+                            i = i.replace("\n", "").split("|")
                             allmessid.update({i[0]: i[1]})
                         if str(message.reference.resolved.id) in allmessid.keys():
                             onixgeneral = bot.get_channel(930842597759541328)
@@ -100,26 +105,31 @@ async def on_message(message: discord.Message):
                             await msg.delete()
                             await message.add_reaction("✅")
                             return
-                    except:
+                    except Exception as e:
+                      async with aiohttp.ClientSession() as session:
                         await message.add_reaction("❌")
-                        return
+                        await sendwebhook(content=f"```{e}```")
 
     #reply system
     
     if message.channel.id == endchannel:   
-        if message. reference:
+        if message.reference:
             if message.reference.resolved.author.bot:
-                file = open("list.txt", "r")
-                lines = file.readlines()
+                print("ballsex")
+                lines = open("list.txt", "r").readlines()
                 allmessid = {}
                 for i in lines:
                     i = i.replace("\n", "")
                     i = i.split("|")
                     allmessid.update({i[0]: i[1]})
                 if str(message.reference.resolved.id) in allmessid.keys():
-                    onixgeneral = bot.get_channel(930842597759541328)
-                    msg = await onixgeneral.fetch_message(int(allmessid[str(message.reference.resolved.id)]))
-                    await msg.reply(message.content)
-
+                    try:
+                        onixgeneral = bot.get_channel(930842597759541328)
+                        msg = await onixgeneral.fetch_message(int(allmessid[str(message.reference.resolved.id)]))
+                        await msg.reply(message.content)
+                    except Exception as e:
+                      async with aiohttp.ClientSession() as session:
+                        await message.add_reaction("❌")
+                        await sendwebhook(content=f"```{e}```")
 
 bot.run(token)
